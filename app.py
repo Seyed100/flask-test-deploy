eb534841e3f1b9b8a7492a1d7aafa94d9d979c28
Seyed100 <sydmmui@gmail.com>
2022-05-13 06:47:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
-from flask import Flask, render_template , request
+from flask import Flask
 
-from forms import GetDataForm 
 
 
 app = Flask(__name__)
 
-app.secret_key = "ufsk;gj;sjlflihflslfblsjfe"
 
 @app.route('/')
 def OnIndex():
-    return render_template('Home.html')
-
-@app.route('/login')
-def OnLogin():
-    return render_template('Login.html')
+    return 'Hello Application'
 
 
-@app.route('/getdata' , methods = ['GET' , 'POST'])
-def GetData():
-    form = GetDataForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            print ('Validate Form Successfully')
-            pass
-        else:
-            print('Error in Form')
-    return render_template('GetData.html' , form = form)
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
